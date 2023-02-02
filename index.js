7e690beb971481518421a39d0ead73c255abe414
ClaudioFritzen <sdfritzen96@gmail.com>
2023-02-02 23:46:28 +0000

    criamos uma função para fazer o ativamento da resposta e a outra função é pra deixar apenas uma resposta selecionada

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const ratingEls = document.querySelectorAll('.rating');
 
 ratingEls.forEach((ratingEl) => {
     ratingEl.addEventListener("click", (event) => {
-        console.log(event.target.innerText || event.target.parentNode.innerText);
+        console.log(event.target.innerText || 
+            event.target.parentNode.innerText);
+        event.target.classList.add('active')
+        event.target.parentNode.classList.add('active')
     })
 })
+
+
+// função para remover o selecionado 
+function removeActive(){
+    ratingEls.forEach((ratingEl) => {
+        ratingEl.classList.remove('active')
+    })
+}
