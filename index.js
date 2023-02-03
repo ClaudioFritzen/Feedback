cc66b6ba89de61168f86f40a8f4918fbdf967853
ClaudioFritzen <sdfritzen96@gmail.com>
2023-02-03 00:22:04 +0000

    conseguimos selecionar a opção e mandamos uma mensagem personalizada

--- index.js
+++ index.js
@@ -3,13 +3,37 @@
 
 const ratingEls = document.querySelectorAll('.rating');
 
+const bntEl = document.getElementById('btn')
+
+const containerEl = document.getElementById('container')
+
+let selectionRating = "";
+
 ratingEls.forEach((ratingEl) => {
     ratingEl.addEventListener("click", (event) => {
-        console.log(event.target.innerText || 
-            event.target.parentNode.innerText);
-        event.target.classList.add('active')
-        event.target.parentNode.classList.add('active')
-    })
+
+        // importamos a função abaixo
+        removeActive();
+        selectionRating = 
+            event.target.innerText || 
+            event.target.parentNode.innerText;
+        /* console.log(event.target.innerText || 
+            event.target.parentNode.innerText); */
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+    });
+});
+
+// criando uma ação quando o usuario clicar no enviar 
+bntEl.addEventListener("click", () => {
+    if(selectionRating !== ""){
+        containerEl.innerHTML = `
+        <strong> Thank You! </strong>
+        <br><br>
+        <strong> Feedback: ${selectionRating} </strong>
+        <p>  Seu voto ajuda a nos motivar a melhorar. Obrigado. </p>
+        `
+    }
 })
 
 
